4108323526ce5d3db8a1d3ef41233d6bb7b893fb
redwantests <bobmorane045@gmail.com>
2024-10-29 13:00:34 +0100

    Question 5, collection_detail

--- collec_management/views.py
+++ collec_management/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from .models import Collec
 # Create your views here.
 def about(request):
     return render(request, 'collec_management/about.html')
 
 def collection_detail(request, id):
-    collection = get_object_or_404(Collec, id=id)
+    try:
+        collection=Collec.objects.get(pk=id)
+    except Collec.DoesNotExist:
+        raise Http404("La collection n'existe pas")
     return render(request, 'collec_management/collection_detail.html', {'collection': collection})
 
-def all(request):
-    collections=Collec.objects.all()
-    return render(request, "collec_management/colleclist.html", {"collections":collections})
